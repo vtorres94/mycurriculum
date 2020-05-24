c092ceb5b12e4b7505ec611a71e5546378cf959f
Vladimir Torres <vladimir.tordavi@gmail.com>
2020-05-24 16:46:01 -0500

    se agrego animación de carga

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './layout/header';
 import Body from './layout/body';
 import Footer from './layout/footer';
@@ -7,26 +7,43 @@ import './App.css';
 
 function App() {
   const [ language, setLanguage ] = useState(true);
+  const [ loading, setLoading ] = useState(true);
   const handleLanguage = () => {
     setLanguage(!language);
     console.log("pues hay va la cosa" + language);
   }
-  return (
-    <div className="App">
-      {document.onload ?
+  const components = () => {
+      return(
+        <div className="App" visible={false}>
+
+        <div>
+          <Header handleLanguage={handleLanguage}/>
+          <Body language={language}/>
+          <Footer/>
+        </div>
+        </div>
+      )
+  }
+  useEffect(() => {
+    // Update the document title using the browser API
+    // if(loading){setLoading(true);}
+    window.onloadeddata = setLoading(false)
+  });
+  const load = () => {
+    return (
+      <div className="App">
         <div className='App-header'>
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-      :(
-        <div>
-        <Header handleLanguage={handleLanguage}/>
-        <Body language={language}/>
-        <Footer/>
-        </div>
-      )
-      }
-      
-    </div>
+      </div>
+    )
+  }
+
+  const app = () => {
+    if(loading) { return(load()) } else { return(components())}
+  }
+  return (
+    app()
   );
 }
 
